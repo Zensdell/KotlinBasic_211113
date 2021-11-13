8a88602b4708c16f9be6610bb977967c5f9178e2
구영모 <dudah754@naver.com>
2021-11-13 10:52:07 +0900

    에어로그 찍어보기

--- app/src/main/java/com/nepplus/kotlinbasic_211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_211113/MainActivity.kt
@@ -2,12 +2,21 @@ package com.nepplus.kotlinbasic_211113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+            //            이 내부의 코드는, 로그찍기 버튼을 누를때마다 실행됨.
+
+            Log.d("메인화면","로그찍기 버튼 누름")
+            Log.e("메인화면", "e로 로그찍어보기")
+            Log.
+        }
 
 
 
