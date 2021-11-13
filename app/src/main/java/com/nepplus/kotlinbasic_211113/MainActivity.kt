e61d3f8ca42228bac81102e3c82cf9c9d8e2fdcb
구영모 <dudah754@naver.com>
2021-11-13 10:28:15 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/kotlinbasic_211113/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nepplus.kotlinbasic_211113
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
